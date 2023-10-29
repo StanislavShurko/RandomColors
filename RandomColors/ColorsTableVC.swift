346da96386eb094840aaf5599ea747dd392702fe
Stanislav Shurko <stanislav.shurko@coffeecup.app>
2023-10-29 11:36:43 +0100

    Randdom Colors finished

--- RandomColors/ColorsTableVC.swift
+++ RandomColors/ColorsTableVC.swift
@@ -8,22 +8,55 @@
 import UIKit
 
 class ColorsTableVC: UIViewController {
+    
+    enum Cells {
+        static let colorCell = "colorCell";
+    }
+    
+    enum Segues {
+        static let toDetail = "ToColorsDetailVC";
+    }
+    
+    var colors: [UIColor] = [];
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.colors = createRandomColorsArray();
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func createRandomColorsArray() -> [UIColor] {
+        var randomColors: [UIColor] = [];
+        for _ in 0..<50 {
+            randomColors.append(.random());
+        }
+        return randomColors
     }
-    */
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destVC = segue.destination as! ColorsDetailVC;
+        destVC.color = sender as? UIColor;
+    }
 
+}
+
+extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.colors.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Cells.colorCell, for: indexPath);
+        cell.backgroundColor = self.colors[indexPath.row];
+        return cell;
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let color = self.colors[indexPath.row];
+        performSegue(withIdentifier: Segues.toDetail, sender: color);
+    }
+    
 }
